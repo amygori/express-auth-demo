b7b53e1b9b84304469c9166b44bcd006424319c2
Amy Gori <agori@adwerx.com>
2017-06-26 10:54:10 -0400

    initial commit

---
+++ userData.js
@@ -0,0 +1,16 @@
+module.exports = {
+  users: [
+    {
+      username: "amy",
+      password: "chihuahua"
+    },
+    {
+      username: "grayson",
+      password: "dinotrux"
+    },
+    {
+      username: "lilamae",
+      password: "aye-aye"
+    }
+  ]
+}
